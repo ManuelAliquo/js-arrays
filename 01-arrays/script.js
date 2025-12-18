3688a8b6239bfb259eb9e63a2bcd51dead92ede6
ManuelAliquo <manuelaliquo2103@gmail.com>
2025-12-18 15:12:12 +0100

    task 4

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -19,7 +19,11 @@ teachers.pop();
 console.table(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers[0];
+console.log("firstTeacher:", firstTeacher);
+
+teachers.shift();
+console.table(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
