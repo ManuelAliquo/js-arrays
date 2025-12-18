973d7f38f6a0d2b2f014e44e5030993d51039291
ManuelAliquo <manuelaliquo2103@gmail.com>
2025-12-18 14:48:07 +0100

    task 1

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -1,8 +1,10 @@
 const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
+console.table(teachers);
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+const fourthTeacher = teachers[3];
+console.log(teachers[3]);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
