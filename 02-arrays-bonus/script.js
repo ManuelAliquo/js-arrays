013c4c94a69b0f41e204da954cdacb1f949aa6a9
ManuelAliquo <manuelaliquo2103@gmail.com>
2025-12-18 17:01:01 +0100

    (per ticket)

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -1,16 +1,20 @@
-const teachers = [
-  'Nathan',
-  'Ed',
-  'Fabio',
-  'Phil',
-  'Carlo',
-  'Lewis',
-  'Luca'
-]; // NON MODIFICARE QUESTA VARIABILE
+const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
+console.table(teachers);
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+
+// teachers[0] = "Luca";
+// teachers[1] = "Lewis";
+// teachers[2] = "Carlo";
+// teachers[3] = "Phil";
+// teachers[4] = "Fabio";
+// teachers[5] = "Ed";
+// teachers[6] = "Nathan";
+// console.table(teachers);
+
+const reversedTeachers = teachers.reverse();
+console.table(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
@@ -22,5 +26,6 @@ const longNames = null;
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = null;
 
-// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
+// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa
+// separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
