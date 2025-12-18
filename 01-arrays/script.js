a3e10522c86f0a68bbac914a33050601bc7aabd0
ManuelAliquo <manuelaliquo2103@gmail.com>
2025-12-18 15:13:03 +0100

    task 5

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -26,6 +26,8 @@ teachers.shift();
 console.table(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa");
+console.table(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
